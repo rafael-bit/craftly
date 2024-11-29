be6aa8c7677054f3e34c6baeac2fcac9a8c74c1a
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2024-11-29 12:12:23 -0300

    refactor: remodel the header

---
+++ app/component/page.tsx
@@ -0,0 +1,7 @@
+import Table from "../components/Table";
+
+export default function Component() {
+	return (
+		<Table />
+	)
+}
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -19,7 +19,6 @@ import {
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import Image from 'next/image'
 import Link from 'next/link'
-import { products, callsToAction } from '@/constants/header'
 
 export default function Header() {
 	const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -62,52 +61,12 @@ export default function Header() {
 					>
 						Docs
 					</Link>
-					<Popover className="relative">
-						<PopoverButton
-							className={`outline-none flex items-center gap-x-1 text-sm/6 font-semibold text-primary transition-all duration-300 transform hover:-translate-y-1 hover:text-hover ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-5'
-								} delay-[50ms]`}
-						>
-							Components
-							<ChevronDownIcon aria-hidden="true" className="size-5 flex-none" />
-						</PopoverButton>
-
-						<PopoverPanel
-							transition
-							className="absolute -left-40 top-full z-[10000] mt-3 w-screen max-w-md overflow-hidden bg-primary rounded-3xl shadow-lg ring-1 ring-gray-300/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
-						>
-							<div className="p-4">
-								{products.map((item) => (
-									<div
-										key={item.name}
-										className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm/6 hover:bg-hover"
-									>
-										<div className="flex size-11 flex-none items-center justify-center rounded-lg bg-light group-hover:bg-lightHover">
-											<item.icon aria-hidden="true" className="size-6 text-gray-600 group-hover:text-blue-700" />
-										</div>
-										<div className="flex-auto">
-											<Link href={item.href} className="block font-semibold text-primary">
-												{item.name}
-												<span className="absolute inset-0" />
-											</Link>
-											<p className="mt-1 text-gray-400">{item.description}</p>
-										</div>
-									</div>
-								))}
-							</div>
-							<div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-primary">
-								{callsToAction.map((item) => (
-									<Link
-										key={item.name}
-										href={item.href}
-										className="flex items-center justify-center gap-x-2.5 p-3 text-sm/6 font-semibold text-primary hover:text-blue-600 hover:bg-hover"
-									>
-										<item.icon aria-hidden="true" className="size-5 flex-none text-blue-600" />
-										{item.name}
-									</Link>
-								))}
-							</div>
-						</PopoverPanel>
-					</Popover>
+					<Link
+						href="/component"
+						className={`outline-none flex items-center gap-x-1 text-sm/6 font-semibold text-primary transition-all duration-300 transform hover:-translate-y-1 hover:text-hover ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-5'
+							} delay-[50ms]`}
+					>Components
+					</Link>
 					<Link
 						href="/"
 						className={`text-sm/6 font-semibold text-primary transition-all duration-300 transform hover:-translate-y-1 hover:text-hover ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-5'
@@ -143,21 +102,10 @@ export default function Header() {
 						</div>
 
 						<div className="space-y-3">
-							<Disclosure as="div">
-								<Link href="#" className="block px-3 py-2 text-primary font-semibold rounded-lg hover:bg-hover hover:text-hover">Docs</Link>
-								<DisclosureButton className="w-full text-left text-primary font-semibold rounded-lg hover:bg-hover hover:text-hover p-3">
-									Components <ChevronDownIcon aria-hidden="true" className="inline w-5 h-5 ml-1" />
-								</DisclosureButton>
-								<DisclosurePanel className="pl-6 space-y-2">
-									{[...products, ...callsToAction].map((item) => (
-										<Link key={item.name} href={item.href} className="block px-3 py-2 text-primary font-semibold rounded-lg hover:bg-hover hover:text-hover">
-											{item.name}
-										</Link>
-									))}
-								</DisclosurePanel>
-							</Disclosure>
-							<Link href="#" className="block px-3 text-primary font-semibold rounded-lg hover:bg-hover hover:text-hover">Block</Link>
-							<Link href="#" className="block px-3 text-primary font-semibold rounded-lg hover:bg-hover hover:text-hover">Theme</Link>
+							<Link href="#" className="block px-3 py-2 text-primary font-semibold rounded-lg hover:bg-hover hover:text-hover">Docs</Link>
+							<Link href="/component" className="block px-3 py-2 text-primary font-semibold rounded-lg hover:bg-hover hover:text-hover">Components</Link>
+							<Link href="#" className="block px-3 py-2 text-primary font-semibold rounded-lg hover:bg-hover hover:text-hover">Block</Link>
+							<Link href="#" className="block px-3 py-2 text-primary font-semibold rounded-lg hover:bg-hover hover:text-hover">Theme</Link>
 						</div>
 					</DialogPanel>
 				</div>
