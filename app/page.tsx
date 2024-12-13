f54a0fa85d76e22c45b33505026e25cb6d7188b3
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2024-12-13 15:42:27 -0300

    feat: add resend

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,32 @@ import { PlayCircleIcon } from "@heroicons/react/20/solid";
 import { SiAzuredataexplorer } from "react-icons/si";
 
 export default function Home() {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
   const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('/api/send', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        setMessage('Email sent successfully!');
+        setEmail('');
+      } else {
+        setMessage(`Error: ${data.message}`);
+      }
+    } catch (error) {
+      console.error(error);
+      setMessage('An error occurred while sending the email.');
+    }
+  };
 
   useEffect(() => {
     setIsLoaded(true);
@@ -14,7 +39,7 @@ export default function Home() {
 
   return (
     <>
-      <div id="hero" className="w-[90%] xs:w-[95%] mx-auto m-5 flex items-center justify-center h-[70vh]">
+      <div id="hero" className="w-[90%] xs:w-[95%] mx-auto m-5 flex items-center justify-center h-[75vh]">
         <div
           className={`w-full flex items-start xs:justify-center transition-all duration-1000 ease-out ${isLoaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
             }`}
@@ -53,11 +78,36 @@ export default function Home() {
             <p className="text-xs lg:text-base">
               Bring your ideas to life with our collection of highly customizable components, optimized for speed, accessibility, and design. Compose full pages in minutes, save time, and focus on what truly matters: the user experience.
             </p>
-
             <p className="text-xs lg:text-base font-medium md:font-bold pt-3">
               Elevate your projects. Create with ease. Lead the digital future.
             </p>
           </div>
+        </div>
+      </div>
+      <div className="mb-28 relative w-[80%] mx-auto">
+        <div className="absolute inset-0 rounded-full bg-gradient-to-r from-blue-900 via-blue-800 to-blue-700 blur-3xl opacity-20 transition-transform duration-500 -z-10"></div>
+        <div className="flex flex-col justify-center items-center text-primary px-4 z-10">
+          <h1 className="text-4xl font-bold text-center">Sign up for our newsletter</h1>
+          <p className="text-center w-full max-w-lg py-6">
+            Do you want to be notified when a new component is added to Craftly? Sign up for our newsletter and you’ll
+            be among the first to find out about new features.
+          </p>
+          <form onSubmit={handleSubmit} className="w-full max-w-md">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
+              className="outline-none border border-neutral-600 rounded-lg text-sm bg-neutral-800 text-primary p-2 w-full"
+            />
+            <button
+              type="submit"
+              className="mt-4 bg-blue-700 hover:bg-blue-600 text-white p-2 rounded-lg w-full"
+            >
+              Sign Up
+            </button>
+          </form>
+          {message && <p className="mt-4 text-sm text-green-500">{message}</p>}
         </div>
       </div>
     </>
