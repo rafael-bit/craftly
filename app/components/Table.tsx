aba6a28f4b59061a8a08b892e187eb014b4c7f3e
Rafael Dias <67400064+rafael-bit@users.noreply.github.com>
2025-01-20 11:25:58 -0300

    chore: create a modular component codeViwer

--- app/components/Table.tsx
+++ app/components/Table.tsx
@@ -3,14 +3,10 @@ import Tasks from "./Tasks";
 import SettingsForm from "./SettingsForm";
 import Authenticator from "./Authenticator";
 import Dashboard from "./Dashboard";
-import { RiCodeBoxLine } from "react-icons/ri";
-import { IoIosClose } from "react-icons/io";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { nightOwl } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import Link from "next/link";
+import CodeViewer from "./ui/codeViewer";
 
-const COMPONENT_CODES = {
-	Header: `
+const componentCodeHeader = `
     import Link from 'next/link';
     export default function Header() {
       return (
@@ -26,32 +22,30 @@ const COMPONENT_CODES = {
         </header>
       );
     }
-  `,
-	Footer: `
-    import Link from 'next/link';
-    export default function Footer() {
-      return (
-        <footer className="bg-neutral-900 text-neutral-300">
-          <div className="container mx-auto px-4 py-6 text-center">
-            <p className="text-sm">&copy; {new Date().getFullYear()} MyApp. All rights reserved.</p>
-            <div className="flex justify-center space-x-4 mt-2">
-              <Link href="#" className="hover:text-neutral-100 transition">Privacy Policy</Link>
-              <Link href="#" className="hover:text-neutral-100 transition">Terms of Service</Link>
-            </div>
-          </div>
-        </footer>
-      );
-    }
-  `,
-};
+  `
+
+const componentCodeFooter = `
+	import Link from 'next/link';
+	export default function Footer() {
+		return (
+			<footer className="bg-neutral-900 text-neutral-300">
+				<div className="container mx-auto px-4 py-6 text-center">
+					<p className="text-sm">&copy; {new Date().getFullYear()} MyApp. All rights reserved.</p>
+					<div className="flex justify-center space-x-4 mt-2">
+						<Link href="#" className="hover:text-neutral-100 transition">Privacy Policy</Link>
+						<Link href="#" className="hover:text-neutral-100 transition">Terms of Service</Link>
+					</div>
+				</div>
+			</footer>
+		);
+	}
+`
 
 export default function Table() {
 	const [selectedContent, setSelectedContent] = useState<
 		"General" | "Dashboards" | "Forms" | "Authenticator" | "Tasks" | "Header" | "Footer"
 	>("General");
 	const [isVisible, setIsVisible] = useState(false);
-	const [isCodeVisible, setIsCodeVisible] = useState(false);
-	const [activeCode, setActiveCode] = useState<string | null>(null);
 	const ref = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
@@ -63,38 +57,6 @@ export default function Table() {
 		return () => observer.disconnect();
 	}, []);
 
-	const handleShowCode = (code: string) => {
-		setActiveCode(code);
-		setIsCodeVisible(true);
-	};
-
-	const renderCodeModal = () => (
-		isCodeVisible && activeCode && (
-			<div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-				<div className="bg-zinc-900 rounded-lg shadow-lg p-6 w-3/4 h-2/3 overflow-auto">
-					<button
-						onClick={() => setIsCodeVisible(false)}
-						className="absolute top-4 right-6 text-gray-200 hover:text-gray-100"
-					>
-						<IoIosClose size={24} />
-					</button>
-					<div className="flex justify-between items-center mb-4">
-						<h2 className="text-lg font-bold text-gray-100">Component Code</h2>
-						<button
-							onClick={() => navigator.clipboard.writeText(activeCode)}
-							className="bg-neutral-950 hover:bg-neutral-800 transition-all duration-300 text-white py-2 px-4 rounded-md"
-						>
-							Copy Code
-						</button>
-					</div>
-					<SyntaxHighlighter language="typescript" style={nightOwl}>
-						{activeCode}
-					</SyntaxHighlighter>
-				</div>
-			</div>
-		)
-	);
-
 	const sections = {
 		General: [
 			<Dashboard key="dashboard" />, <SettingsForm key="form" />, <Tasks key="tasks" />, <Authenticator key="auth" />,],
@@ -104,13 +66,8 @@ export default function Table() {
 		Tasks: [<Tasks key="tasks" />],
 		Header: [
 			<div key="header" className="scrollbar">
-				<button
-					onClick={() => handleShowCode(COMPONENT_CODES.Header)}
-					className="mb-3 flex items-center p-2 text-sm bg-neutral-900 hover:bg-neutral-800 transition-all text-white rounded shadow-md"
-				>
-					<RiCodeBoxLine size={24} />
-					View Code
-				</button>
+				<CodeViewer componentCode={componentCodeHeader} />
+
 				<header className="bg-neutral-900 text-neutral-50 shadow-md">
 					<div className="container mx-auto px-4 py-4 flex justify-between items-center">
 						<h1 className="text-2xl font-bold">MyApp</h1>
@@ -125,13 +82,8 @@ export default function Table() {
 		],
 		Footer: [
 			<div key="footer" className="scrollbar">
-				<button
-					onClick={() => handleShowCode(COMPONENT_CODES.Footer)}
-					className="mb-3 flex items-center p-2 text-sm bg-neutral-900 hover:bg-neutral-800 transition-all text-white rounded shadow-md"
-				>
-					<RiCodeBoxLine size={24} />
-					View Code
-				</button>
+				<CodeViewer componentCode={componentCodeFooter} />
+				
 				<footer className="bg-neutral-900 text-neutral-300">
 					<div className="container mx-auto px-4 py-6 text-center">
 						<p className="text-sm">&copy; {new Date().getFullYear()} MyApp. All rights reserved.</p>
@@ -152,7 +104,6 @@ export default function Table() {
 			className={`bg-neutral-950 bg-opacity-35 pb-5 transform transition-all duration-1000 ${isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
 				}`}
 		>
-			{renderCodeModal()}
 			<div className="flex w-1/2 justify-evenly py-4">
 				{Object.keys(sections).map((key) => (
 					<button
